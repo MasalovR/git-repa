15dd3e2950ed6c6e1f68936b8b0dc2590fbdb5ec
Roman <rmasalov@inbox.ru>
2018-01-04 19:09:30 +0600

    добавил точку с запятой

--- CSharProgram/CSharProgram/Program.cs
+++ CSharProgram/CSharProgram/Program.cs
@@ -10,7 +10,7 @@ namespace CSharProgram
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!;");
         }
     }
 }
